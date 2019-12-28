12da8a0e5af825987f7b54733be0e8730400cacc
Alp Akpinar <aakpinar@bu.edu>
2019-12-28 18:01:43 +0300

    Add README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+## ScrapeApp
+
+This application is a result of a Web Scraping/Tkinter exercise in Python. It scrapes data from Wikipedia for the given artist, finds the discography and presents it in a user-friendly format.
+
+To launch the application start window, execute the file `app.py`, located in the root directory:
+
+```
+./app.py
+```
+
+From this page, one can go to the main application page by following the instructions on the GUI. To search for an artist, the user needs to enter the artist name into the entry box in the main page (first letters in capital), and the discography will show up as an output.
--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
 
-from lib.gui import ScrapeApp, StartWindow
+##########################
+# MAIN EXE FOR SCRAPEAPP
+# AUTHOR: Alp Akpinar
+# 
+# This app is a result of Web Scraping/Tkinter 
+# exercise in Python. This application scrapes
+# the data in Wikipedia for the given artist and 
+# extracts the discography, and presents it in a 
+# user friendly format.
+# 
+# Execute this file to launch the start window/main page
+# of the application. From this page, one can have access
+# to the application page where artists can be searched to 
+# get albums.
+##########################
+
+from lib.gui import StartWindow
 
 def main():
-	# Launch start window
+	
 	start_window = StartWindow()
 	start_window.launch_start_window()
 
